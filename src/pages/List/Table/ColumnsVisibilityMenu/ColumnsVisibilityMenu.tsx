580935077b2ae459522e40425a2402d1d1d6c402
Jelizaveta Potapova <jelizaveta.p@pvcase.com>
2025-05-19 21:56:51 +0300

    style: update table menu styling

--- src/pages/List/Table/ColumnsVisibilityMenu/ColumnsVisibilityMenu.tsx
+++ src/pages/List/Table/ColumnsVisibilityMenu/ColumnsVisibilityMenu.tsx
@@ -23,12 +23,36 @@ export const ColumnsVisibilityMenu = ({
   onColumnToggle,
 }: ColumnsVisibilityMenuProps) => {
   return (
-    <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={onMenuClose}>
+    <Menu
+      anchorEl={anchorEl}
+      open={Boolean(anchorEl)}
+      onClose={onMenuClose}
+      slotProps={{
+        root: { sx: { ".MuiList-root": { padding: 0 } } },
+      }}
+      transformOrigin={{ vertical: -8, horizontal: 58 }}
+    >
       {columns.map((column) => (
         <MenuItem
           key={column.field}
           onClick={() => {
             onColumnToggle(column.field);
+          }}
+          sx={{
+            padding: 0,
+            borderBottom: "1px solid rgba(0, 0, 0, 0.12)",
+            "&:last-child": {
+              borderBottom: "none",
+            },
+            "& .MuiFormControlLabel-root": {
+              margin: 0,
+            },
+            "& .MuiListItemIcon-root": {
+              maxWidth: 42,
+            },
+            "& .MuiListItemText-root": {
+              padding: "8px 16px 8px 0",
+            },
           }}
         >
           <ListItemIcon>
